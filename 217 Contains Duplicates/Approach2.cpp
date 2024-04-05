269ec16b848199e0a7f2d8bcaf8c591f4c7a9ea5
Pradeep Rekapalli <rekkapallipradeepg@gmail.com>
2024-04-05 12:45:26 -0400

    Create Approach2.cpp
    Added another approach 2 using maps

---
+++ 217 Contains Duplicates/Approach2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+
+        for(int x:nums){
+            m[x]++;
+            if(m[x]>1){
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+};
