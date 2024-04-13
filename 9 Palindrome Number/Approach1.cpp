398f0e555c17431b14474810052f6f8266b39c12
Pradeep Rekapalli <rekkapallipradeepg@gmail.com>
2024-04-12 22:40:52 -0400

    Create Approach1.cpp

---
+++ 9 Palindrome Number/Approach1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        if(x<=9) return true;
+
+        long long num=0;
+        int temp=x;
+        int k=0;
+        while(temp){
+            temp=temp/10;
+            k++;
+        }
+        temp=x;
+        while(temp){
+            int digit=temp%10;
+            temp=temp/10;
+
+            num+=(digit*pow(10,k-1));
+            k--;
+            
+        }
+
+        cout<<num;
+
+        return num==x;
+    }
+};
